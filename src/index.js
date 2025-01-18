f67df500f9fe9ca5a7b939d9a6f43b8ef4ba9dc1
schunsie <185822585+schunsie@users.noreply.github.com>
2025-01-18 21:42:42 +0100

    Add basic task list logic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
-console.log('hello world!');
+class Task {
+    constructor(title, desc, dueDate, priority) {
+        this.title = title;
+        this.desc = desc;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.completed = false;
+    };
+
+    toggleCompleted() {
+        this.completed = !this.completed;
+    }
+};
+
+class Project {
+    constructor(name){
+        this.name = name;
+        this.tasks = [];
+    };
+};
+
+function addTaskToProject(task, project=defaultList) {
+    project.tasks.push(task);
+}
+
+function removeTaskFromProject(task, project=defaultList) {
+    project.tasks = project.tasks.filter((projectTask) => projectTask !== task);
+};
+
+
+
+
+
+// Testing
+const test = new Task('Buy milk', 'Buy 2 liters of milk', '20-01-2025', 8);
+console.log(test);
+
+test.toggleCompleted()
+console.log(test);
